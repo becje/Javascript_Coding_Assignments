23aab38d697b8c97401befb82b314ba18fa9c0ad
becje <jeff.beck@b5connections.com>
2021-04-27 22:51:10 -0500

    Delete Week-4-CodingAssignments directory

--- Week-4-CodingAssignments/Q3a_setTimeout.js
+++
@@ -1,5 +0,0 @@
-/* Question 3a: Using setTimeout, write an anonymous (has no name assigned to it)
-arrow function in the callback parameter position. The function should alert 
-‘Time is up!’. Choose whatever length of time you want for the timeout. */
-
-setTimeout(() => alert('Time is up!'), 5000);
--- Week-4-CodingAssignments/Q3b_AreWeThereYet.js
+++
@@ -1,8 +0,0 @@
-/* Question 3b: Write an arrow function named askAreWeThereYet
-that alerts ‘Are we there yet?’. Using setInterval, pass askAreWeThereYet
-into the callback parameter position. Choose whatever length of time you 
-want for the interval. */
-
-const askAreWeThereYet = () => alert('Are we there yet');
-
-setInterval(askAreWeThereYet, 5000);
